228457549df5fcfebf012e416d53088bdc5e6b1c
Jeú Chaves <jeuchaveslima@gmail.com>
2024-02-08 15:33:08 -0400

    feat: Instala MUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/home" element={<p>Página Inicial</p>}/>
+            <Route path="/home" element={<Button>Botão</Button>}/>
             <Route path="*" element={<Navigate to="/home"/>}/>
         </Routes>
     );
