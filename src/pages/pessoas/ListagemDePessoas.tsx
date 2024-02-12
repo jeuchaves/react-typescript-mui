ec31c3a6ed05644e866435be66d88f7d56ba8aa2
Jeú Chaves <jeuchaveslima@gmail.com>
2024-02-12 14:38:17 -0400

    feat: Cria hook para debounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -3,35 +3,39 @@ import { FerramentasDaListagem } from "../../shared/components"
 import { LayoutBaseDePagina } from "../../shared/layouts"
 import { useEffect, useMemo } from "react";
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { useDebounce } from "../../shared/hooks";
 
 export const ListagemDePessoas: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const { debounce } = useDebounce();
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
-    }, [searchParams])
+    }, [searchParams]);
 
     useEffect(() => {
-        PessoasService.getAll(1, busca)
-            .then((result) => {
-                if (result instanceof Error) {
-                    alert(result.message);
-                    return;
-                }
-                console.log(result.data);
-            });
-    }, [busca])
+        debounce(() => {
+            PessoasService.getAll(1, busca)
+                .then((result) => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                        return;
+                    }
+                    console.log(result);
+                });
+        });
+    }, [busca, debounce])
 
     return (
-        <LayoutBaseDePagina 
+        <LayoutBaseDePagina
             titulo="Listagem de pessoas"
             barraDeFerramentas={
                 <FerramentasDaListagem
                     textoBotaoNovo="Nova"
                     mostrarInputBusca
                     textoDaBusca={busca}
-                    aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, {replace: true})}
+                    aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
                 />
             }
         >
